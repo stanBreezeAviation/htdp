affc4992930cbdd3598deae0a1f08032362fad2c
stanBreezeAviation <70974087+stanBreezeAviation@users.noreply.github.com>
2021-09-29 14:56:47 -0600

    Updates

--- zoo_animals.rkt
+++ zoo_animals.rkt
@@ -1,5 +1,13 @@
 #lang racket
 ;; Data Definition
+;; a zoo-animal is either
+;; (make-spider LEGS SPACE)
+;; (make-elephants  SPACE)
+;; (make-monkey INTELLIGENCE SPACE)
+;; where legs and space is a number, and
+;; INTELLIGENCE is a symbol, one of
+;; 'homer 'marge 'lisa (indicating dumb, average, or smart respectively)
+
 (define-struct spider (legs space))
 (define-struct elephant (space))
 (define-struct monkey (intelligence space))
